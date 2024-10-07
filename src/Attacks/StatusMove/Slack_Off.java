74394ac33521ceee08583850fa2ef4a3724360ec
dmitrykalinin5 <dkalinin240@gmail.com>
2024-10-07 18:09:40 +0300

    Start

--- src/Attacks/StatusMove/Slack_Off.java
+++ src/Attacks/StatusMove/Slack_Off.java
@@ -3,8 +3,7 @@ import ru.ifmo.se.pokemon.*;
 
 public class Slack_Off extends StatusMove {
     @Override
-    protected void applySelfEffects(Pokemon var1) {
-        // Сомнительно (нужно отхилить на половину от макс здоровья)
+    protected void applySelfEffects(Pokemon var1)  {
         Effect hp = (new Effect());
         hp.stat(Stat.HP, (int) var1.getHP() / 2);
     }
